afef47e830dc585b084481ba85a6ff44899bf9a0
Michael van Straten <mvanstraten@mozilla.com>
2025-04-16 23:45:50 +0200

    Refactor UpdateableZipFile to use open() for file operations; fixes #1

--- src/updateable_zip_file/__init__.py
+++ src/updateable_zip_file/__init__.py
@@ -1,7 +1,6 @@
 import os
-import shutil
-import tempfile
-from zipfile import ZipFile, ZIP_STORED, ZipInfo
+from tempfile import NamedTemporaryFile, mktemp
+from zipfile import ZipFile, ZipInfo
 
 
 class UpdateableZipFile(ZipFile):
@@ -11,54 +10,28 @@ class UpdateableZipFile(ZipFile):
     Upon  __exit__ (if updates were applied) a new zip file will override the exiting one with the updates
     """
 
-    class DeleteMarker(object):
-        pass
-
-    def __init__(self, file, mode="r", compression=ZIP_STORED, allowZip64=False):
-        # Init base
-        super(UpdateableZipFile, self).__init__(
-            file, mode=mode, compression=compression, allowZip64=allowZip64
-        )
-        # track file to override in zip
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self._replace = {}
-        # Whether the with statement was called
+        self._delete = set()
         self._allow_updates = False
 
-    def writestr(self, zinfo_or_arcname, bytes, compress_type=None):
-        if isinstance(zinfo_or_arcname, ZipInfo):
-            name = zinfo_or_arcname.filename
-        else:
-            name = zinfo_or_arcname
-        # If the file exits, and needs to be overridden,
-        # mark the entry, and create a temp-file for it
-        # we allow this only if the with statement is used
-        if self._allow_updates and name in self.namelist():
-            temp_file = self._replace[name] = self._replace.get(
-                name, tempfile.TemporaryFile()
-            )
-            temp_file.write(bytes)
-        # Otherwise just act normally
-        else:
-            super(UpdateableZipFile, self).writestr(
-                zinfo_or_arcname, bytes, compress_type=compress_type
-            )
+    def open(self, name, mode="r", pwd=None, *, force_zip64=False):
+        if replacement := self._replace.get(name, None):
+            return replacement
 
-    def write(self, filename, arcname=None, compress_type=None):
-        arcname = arcname or filename
-        # If the file exits, and needs to be overridden,
-        # mark the entry, and create a temp-file for it
-        # we allow this only if the with statement is used
-        if self._allow_updates and arcname in self.namelist():
-            temp_file = self._replace[arcname] = self._replace.get(
-                arcname, tempfile.TemporaryFile()
-            )
-            with open(filename, "rb") as source:
-                shutil.copyfileobj(source, temp_file)
-        # Otherwise just act normally
-        else:
-            super(UpdateableZipFile, self).write(
-                filename, arcname=arcname, compress_type=compress_type
-            )
+        if mode == "w" and name in self.namelist():
+            if not self._allow_updates:
+                raise RuntimeError(
+                    "Updates only allowed within a with-statement context"
+                )
+
+            temp_file = self._replace[name] = NamedTemporaryFile(delete=False)
+            return temp_file
+
+        return super(UpdateableZipFile, self).open(
+            name, mode, pwd, force_zip64=force_zip64
+        )
 
     def __enter__(self):
         # Allow updates
@@ -68,54 +41,49 @@ class UpdateableZipFile(ZipFile):
     def __exit__(self, exc_type, exc_val, exc_tb):
         # call base to close zip file, organically
         try:
-            super(UpdateableZipFile, self).__exit__(exc_type, exc_val, exc_tb)
-            if len(self._replace) > 0:
+            if self._replace or self._delete:
+                print(self._replace, self._delete)
                 self._rebuild_zip()
+            else:
+                super().__exit__(exc_type, exc_val, exc_tb)
         finally:
             # In case rebuild zip failed,
             # be sure to still release all the temp files
-            self._close_all_temp_files()
+            self._cleanup_temp_files()
             self._allow_updates = False
 
-    def _close_all_temp_files(self):
+    def _cleanup_temp_files(self):
         for temp_file in self._replace.values():
-            if hasattr(temp_file, "close"):
-                temp_file.close()
+            temp_file.close()
+            os.unlink(temp_file.name)
 
-    def remove_file(self, path):
-        self._replace[path] = self.DeleteMarker()
+    def remove_file(self, filename):
+        self._delete.add(filename)
 
     def _rebuild_zip(self):
-        tempdir = tempfile.mkdtemp()
-        try:
-            temp_zip_path = os.path.join(tempdir, "new.zip")
-            with ZipFile(self.filename, "r") as zip_read:
-                # Create new zip with assigned properties
-                with ZipFile(
-                    temp_zip_path,
-                    "w",
-                    compression=self.compression,
-                    allowZip64=self._allowZip64,
-                ) as zip_write:
-                    for item in zip_read.infolist():
-                        # Check if the file should be replaced / or deleted
-                        replacement = self._replace.get(item.filename, None)
-                        # If marked for deletion, do not copy file to new zipfile
-                        if isinstance(replacement, self.DeleteMarker):
-                            del self._replace[item.filename]
-                            continue
-                        # If marked for replacement, copy temp_file, instead of old file
-                        elif replacement is not None:
-                            del self._replace[item.filename]
-                            # Write replacement to archive,
-                            # and then close it (deleting the temp file)
-                            replacement.seek(0)
-                            data = replacement.read()
-                            replacement.close()
-                        else:
-                            data = zip_read.read(item.filename)
-                        zip_write.writestr(item, data)
-            # Override the archive with the updated one
-            shutil.move(temp_zip_path, self.filename)
-        finally:
-            shutil.rmtree(tempdir)
+        file = mktemp(suffix=".zip")
+        updated_zip = ZipFile(file, mode="w")
+
+        for item in self.infolist():
+            if isinstance(item, ZipInfo):
+                filename = item.filename
+            else:
+                filename = item
+
+
+            # If marked for replacement, copy temp_file, instead of old file
+            if replacement := self._replace.get(filename, None):
+                del self._replace[item.filename]
+                with open(replacement.name) as f:
+                    # Write replacement to archive,
+                    # and then close it (deleting the temp file)
+                    f.seek(0)
+                    data = f.read()
+                os.unlink(replacement.name)
+            elif filename in self._delete:
+                continue
+            else:
+                data = self.read(filename)
+            updated_zip.writestr(item, data)
+
+        os.rename(file, self.filename)
--- tests/test_updateable_zip_file.py
+++ tests/test_updateable_zip_file.py
@@ -10,15 +10,98 @@ def test_overwriting_deleted_file():
 
     with ZipFile(tmp, "w") as zip:
         with zip.open("text.txt", "w") as f:
-            f.write(b"something")
+            f.write(b"original content")
 
     with UpdateableZipFile(tmp, "a") as zip:
         zip.remove_file("text.txt")
         with zip.open("text.txt", "w") as f:
-            f.write(b"something else")
+            f.write(b"updated content")
+
+    with ZipFile(tmp, "r") as zip:
+        with zip.open("text.txt") as f:
+            assert f.read() == b"updated content"
+
+    os.remove(tmp)
+
+
+def test_add_new_file():
+    tmp = tempfile.mktemp(suffix=".zip")
+
+    with ZipFile(tmp, "w") as zip:
+        pass
+
+    with UpdateableZipFile(tmp, "a") as zip:
+        with zip.open("new_file.txt", "w") as f:
+            f.write(b"new file content")
+
+    with ZipFile(tmp, "r") as zip:
+        with zip.open("new_file.txt") as f:
+            assert f.read() == b"new file content"
+
+    os.remove(tmp)
+
+
+def test_delete_existing_file():
+    tmp = tempfile.mktemp(suffix=".zip")
+
+    with ZipFile(tmp, "w") as zip:
+        with zip.open("delete_me.txt", "w") as f:
+            f.write(b"this will be deleted")
+
+    with UpdateableZipFile(tmp, "a") as zip:
+        zip.remove_file("delete_me.txt")
+
+    with ZipFile(tmp, "r") as zip:
+        assert "delete_me.txt" not in zip.namelist()
+
+    os.remove(tmp)
+
+
+def test_multiple_operations():
+    tmp = tempfile.mktemp(suffix=".zip")
+
+    with ZipFile(tmp, "w") as zip:
+        zip.writestr("file1.txt", b"content1")
+        zip.writestr("file2.txt", b"content2")
+
+    with UpdateableZipFile(tmp, "a") as zip:
+        zip.remove_file("file1.txt")
+        with zip.open("file2.txt", "w") as f:
+            f.write(b"updated content2")
+        with zip.open("file3.txt", "w") as f:
+            f.write(b"content3")
+
+    with ZipFile(tmp, "r") as zip:
+        assert "file1.txt" not in zip.namelist()
+        assert zip.read("file2.txt") == b"updated content2"
+        assert zip.read("file3.txt") == b"content3"
+
+    os.remove(tmp)
+
+
+def test_readding_deleted_file():
+    tmp = tempfile.mktemp(suffix=".zip")
+
+    with ZipFile(tmp, "w") as zip:
+        zip.writestr("file.txt", b"initial content")
+
+    with UpdateableZipFile(tmp, "a") as zip:
+        zip.remove_file("file.txt")
+
+    with UpdateableZipFile(tmp, "a") as zip:
+        with zip.open("file.txt", "w") as f:
+            f.write(b"new content after delete")
+
+    with ZipFile(tmp, "r") as zip:
+        assert zip.read("file.txt") == b"new content after delete"
 
     os.remove(tmp)
 
 
 if __name__ == "__main__":
     test_overwriting_deleted_file()
+    test_add_new_file()
+    test_delete_existing_file()
+    test_multiple_operations()
+    test_readding_deleted_file()
+    print("All tests passed!")
